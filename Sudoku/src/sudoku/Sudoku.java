6250c1d374bcfabe3a639abe405f52a1411ecd54
mikeni <mikeni@IWHBYD>
2017-12-08 00:16:59 -0600

    Mensaje sout de prueba en clase sudoku

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sudoku;
 
-/**
- *
- * @author Miguel
- */
 public class Sudoku {
 
     /**
@@ -17,6 +8,7 @@ public class Sudoku {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Ludan");
+        System.out.println("Prueba");
     }
     
 }
