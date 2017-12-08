b8b5818b3f410f3351edb6931e742d107d00640c
mikeni <mikeni@IWHBYD>
2017-12-08 10:07:42 -0600

    T

--- Sudoku/src/sudoku/GUI.java
+++ Sudoku/src/sudoku/GUI.java
@@ -4,37 +4,38 @@ import java.awt.*;
 import java.awt.event.*;
 
 class GUI implements ActionListener {
+    JPanel cont,p,p1;
     static JFrame f;
     static JTextField[] tf = new JTextField[81];
-    JPanel cont,p,p1;
-    static JButton b,b1;
-    static int x,y,size = 9 ;
-    static int[][] sudoku = new int[size][size] ;
-    static double time = 0.00 ;
-    static boolean flag = false ;
+    static JButton b1,b2;
+    static int x,y,tam = 9;
+    static int[][] sudoku=new int[tam][tam];
+    static double tiempo=0.00;
+    static boolean flag=false;
     
     public GUI(){
         f = new JFrame("Resolución del Sudoku");
+        //f.setLocationRelativeTo(null);
         cont = new JPanel();
         cont.setLayout(new BoxLayout(cont,BoxLayout.Y_AXIS));        
-        p = new JPanel(new GridLayout(size,size));
+        p = new JPanel(new GridLayout(tam,tam));
 
-        for(int i = 0 ; i < size ; i++ )
+        for(int i=0; i<tam;i++)
         {
-            for(int j = 0 ; j < size ; j++ )
+            for(int j=0;j<tam;j++)
             {
-                tf[i*size + j] = new JTextField("");
-                p.add(tf[i*size + j]);
+                tf[i*tam + j] = new JTextField("");
+                p.add(tf[i*tam + j]);
             }
         }
         
         p1 = new JPanel();        
-        b = new JButton("Resolver Sudoku");
-        b.addActionListener(this);        
-        p1.add(b);        
-        b1 = new JButton("Reiniciar Sudoku");
+        b1 = new JButton("Resolver Sudoku");
         b1.addActionListener(this);        
         p1.add(b1);        
+        b2 = new JButton("Reiniciar Sudoku");
+        b2.addActionListener(this);        
+        p1.add(b2);        
         cont.add(p);
         cont.add(p1);        
         f.add(cont);        
@@ -45,25 +46,25 @@ class GUI implements ActionListener {
     }
     
     public void actionPerformed(ActionEvent e){
-        if( e.getSource() == b)
+        if( e.getSource() == b1)
         {
             solve();
             if(flag)
-                b.setText("¡Resuelto! Tiempo: "+time+" seg.");
+                b1.setText("¡Resuelto! Tiempo: "+tiempo+" seg.");
             else
-                b.setText("Resolver Sudoku");
+                b1.setText("Resolver Sudoku");
         }
-        if( e.getSource() == b1 )
+        if( e.getSource() == b2 )
             reset();        
     }
     
     
     private static void reset(){
-        for(int i = 0 ; i < size*size ; i++ )
+        for(int i = 0 ; i < tam*tam ; i++ )
         {
             tf[i].setText("");
         }        
-        b.setText("Resolver Sudoku");        
+        b1.setText("Resolver Sudoku");        
         JOptionPane.showMessageDialog(f, "Se ha reiniciado el Sudoku.");
     }
     
@@ -80,22 +81,22 @@ class GUI implements ActionListener {
             if( solveSudoku() )            
                 flag = true ;            
             else            
-                JOptionPane.showMessageDialog(f,"¡Sudoku no válido! Intente de nuevo. Tiempo: "+time+" seg.");            
+                JOptionPane.showMessageDialog(f,"¡Sudoku no válido! Intente de nuevo. Tiempo: "+tiempo+" seg.");            
         }
         else        
             JOptionPane.showMessageDialog(f,"¡Sudoku no válido! Intente de nuevo.");              
-        time = ( System.currentTimeMillis() - t1 )/1000.000 ;
+        tiempo = ( System.currentTimeMillis() - t1 )/1000.000 ;
     }
     
     private static void makeSudoku(){
-        for(int i=0 ; i< size ; i++ )
+        for(int i=0 ; i< tam ; i++ )
         {
-            for(int j=0 ; j< size ; j++ )
+            for(int j=0 ; j< tam ; j++ )
             {
-                if( ( (tf[i*size + j]).getText() ).equals("") )
+                if( ( (tf[i*tam + j]).getText() ).equals("") )
                     sudoku[i][j] = 0 ;
                 else
-                    sudoku[i][j] = (int)( (tf[i*size + j]).getText().charAt(0) ) - 48;
+                    sudoku[i][j] = (int)( (tf[i*tam + j]).getText().charAt(0) ) - 48;
             }
         }
     }
@@ -105,9 +106,9 @@ class GUI implements ActionListener {
         int row = 0 , col = 0;
         boolean f = false;
         //find unassigned location
-        for( row = 0 ; row < size ; row++ )
+        for( row = 0 ; row < tam ; row++ )
         {
-            for( col = 0 ; col < size ; col++ )
+            for( col = 0 ; col < tam ; col++ )
             {
                 if( sudoku[row][col] == 0 )
                 {
@@ -122,14 +123,14 @@ class GUI implements ActionListener {
         if( f == false )     //if no unassigned
             return true ;
         
-        for(int n = 1 ; n <= size ; n++ )
+        for(int n = 1 ; n <= tam ; n++ )
         {
             if( isSafe(row,col,n) )
             {
                 //make assignment
                 sudoku[row][col] = n ;
                 //print output
-                (tf[(row)*size + col]).setText(Integer.toString(n));
+                (tf[(row)*tam + col]).setText(Integer.toString(n));
                
                 //return if success
                 if( solveSudoku() )
@@ -143,11 +144,11 @@ class GUI implements ActionListener {
     }
         
     private static boolean validate(){
-        for(int i=0 ; i<size ; i++ )
+        for(int i=0 ; i<tam ; i++ )
         {
-            for(int j=0 ; j<size ; j++ )
+            for(int j=0 ; j<tam ; j++ )
             {
-                if( sudoku[i][j] < 0 && sudoku[i][j] > size )
+                if( sudoku[i][j] < 0 && sudoku[i][j] > tam )
                     return false ;                
                 if( sudoku[i][j] != 0 && (usedInRow(i,j,sudoku[i][j]) || usedInCol(i,j,sudoku[i][j]) || usedInBox(i,j, sudoku[i][j]) ) )                
                     return false ;                
@@ -163,7 +164,7 @@ class GUI implements ActionListener {
     
     
     private static boolean usedInRow(int r , int c, int n){ 
-        for(int col=0 ; col<size ; col++ )
+        for(int col=0 ; col<tam ; col++ )
         {
             if( col != c && sudoku[r][col] == n )            
                 return true;            
@@ -173,7 +174,7 @@ class GUI implements ActionListener {
     
     
     private static boolean usedInCol(int r,int c , int n){
-        for(int row=0 ; row < size ; row++ )
+        for(int row=0 ; row < tam ; row++ )
         {
             if( row != r && sudoku[row][c] == n )            
                 return true;            
@@ -183,11 +184,11 @@ class GUI implements ActionListener {
     
     
     private static boolean usedInBox(int r , int c , int n){
-        int r_st = r-r%((int)Math.sqrt(size)) ;
-        int c_st = c-c%((int)Math.sqrt(size)) ;        
-        for(int i=0 ; i< (int)Math.sqrt(size) ; i++ )
+        int r_st = r-r%((int)Math.sqrt(tam)) ;
+        int c_st = c-c%((int)Math.sqrt(tam)) ;        
+        for(int i=0 ; i< (int)Math.sqrt(tam) ; i++ )
         {
-            for(int j=0 ; j< (int)Math.sqrt(size) ; j++ )
+            for(int j=0 ; j< (int)Math.sqrt(tam) ; j++ )
             {
                 if( r_st+i != r && c_st+j != c && sudoku[r_st+i][c_st+j] == n )                
                     return true;                
